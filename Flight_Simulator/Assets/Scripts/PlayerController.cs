2c008edf7a4401438574310f78ed862bcd67bc8a
Cameron <cameron2.bell@live.uwe.ac.uk>
2020-11-20 13:11:03 +0000

    Added CineMachine + simple character movement for testing

---
+++ Flight_Simulator/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInput input;
+    private CharacterController controller;
+
+    public float gravity = -9.81f;
+    public float speed = 10.0f;
+    public float velY;
+
+    Vector3 velocity;
+
+    void Start()
+    {
+        controller = FindObjectOfType<CharacterController>();
+        input = FindObjectOfType<PlayerInput>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        if(controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(input.moveCharacter * speed * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
---
+++ Flight_Simulator/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    [HideInInspector] public bool inputJump;
+    [HideInInspector] public bool canInput;
+    [HideInInspector] public bool canAim;
+
+    [HideInInspector] public float horizontal;
+    [HideInInspector] public float vertical;
+    [HideInInspector] public float mouseX;
+    [HideInInspector] public float mouseY;
+    [HideInInspector] public Vector3 moveCharacter;
+
+    public float mouseSensitivity = 100.0f;
+    void Start()
+    {
+        canInput = true;
+        canAim = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        while (canInput)
+        {
+            inputJump = Input.GetButtonDown("Jump");
+            horizontal = Input.GetAxisRaw("Horizontal");
+            vertical = Input.GetAxisRaw("Vertical");
+
+            if (canAim)
+            {
+                mouseX = Input.GetAxisRaw("Mouse X") * mouseSensitivity * Time.deltaTime;
+                mouseY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity * Time.deltaTime;
+            }
+            break;
+        }
+        moveCharacter = transform.right * horizontal + transform.forward * vertical;
+    }
+
+    public void KillMoveInput()
+    {
+
+    }
+
+    public void KillInputs()
+    {
+        inputJump = false;
+
+    }
+}
